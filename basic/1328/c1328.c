120e1d8ab2ab5a92f7e81098ae6a8e3fcf11ea2e
doffery <doffery20@gmail.com>
2015-01-30 22:06:13 +0800

    ac

--- basic/1328/c1328.c
+++ basic/1328/c1328.c
@@ -27,18 +27,21 @@ int main() {
 
 		int num = 0; i = 0;
 		double pd = pow(d, 2);
+		double pre = points[0].x;
 		while(i < n) {
 			if(points[i].y > d || points[i].y < -d) {
 				num = -1;
 				break;
 			}
 			double cx = points[i].x + sqrt(pd - pow(points[i].y, 2));
-
+			if(cx < pre)
+				--num;
 			++i;
 			while(pow(points[i].y, 2) + pow(points[i].x - cx, 2) <= pd
 					&& i < n)
 				++i;
 			++num;
+			pre = cx;
 		}
 		printf("Case %d: %d\n", cases, num);
 	}
