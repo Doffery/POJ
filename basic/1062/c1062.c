7eeb545867bdaeb4973a342fadbd7a16554af076
doffery <doffery20@gmail.com>
2015-02-03 20:03:50 +0800

    continue.

--- basic/1062/c1062.c
+++ basic/1062/c1062.c
@@ -1,57 +1,38 @@
 #include<iostream>
 #include<cstdio>
 #include<cstdlib>
-#include<queue>
+#include<vector>
 
 typedef struct {
-	int money;
 	int index;
-	Edge *next;
+	int money;
 }Edge;
-typedef struct {
+
+struct TNode{
 	int money;
 	int level;
-	Edge *edge;
-
-	bool operator < (const TNode & a) const {
-		return a.money > money;
-	}
-}TNode;
-TNode nodes[110];
-
-int costs[110];
-
+	std::vector<Edge> edges;
+}nodes[110];
 
 int main() {
 	freopen("in.txt", "r", stdin);
 	freopen("out.txt", "w", stdout);
 
-	int m, n;
-	scanf("%d %d", &m, &n);
-
-	int i;
-	for(i = 1; i < n+1; ++i) {
-		int en;
-		scanf("%d %d %d", &nodes[i].money, &nodes[i].level, &en);
-		if(en == 0)
-			nodes[i].edge = NULL;
-		else {
-			Edge e;
-			scanf("%d %d", &e.index, &e.money);
-			nodes[i].edge = e; --en;
-			Edge *pre = &e;
-			int j = 0;
-			for(;en > 0; --en) {
-				Edge ed;
-				scanf("%d %d", &ed.index, &ed.money);
-				pre->next = ed;
-				pre = &ed;
+	int m, n, i, j;
+	while(scanf("%d %d", &m, &n) == 2) {
+		for(i = 1; i <= n; ++i) {
+			int en;
+			scanf("%d %d %d", &nodes.money, &nodes.level, &en);
+			for(j = 0; j < en; ++j) {
+				Edge e;
+				scanf("%d %d", &e.index, &e.money);
+				edges.push_back(e);
 			}
-			pre->next = NULL;
 		}
 	}
 
-	
-	
-
+	return 0;
 }
+
+int dfs
+
